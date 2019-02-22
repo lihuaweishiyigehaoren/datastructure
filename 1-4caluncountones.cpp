9e9b484f852f029ada40f42c94ed5adadc64f40c
lihuawei <1206909328@qq.com>
2019-02-22 21:39:04 +0800

    countones3

--- 1-4caluncountones.cpp
+++ 1-4caluncountones.cpp
@@ -1,11 +1,15 @@
+#include <iostream>
+using namespace std;
+
 // 原始算法
 int countOnes1(unsigned int n)
 {
     int ones = 0;
     while(n>0){
-        ones += (n&1)
+        ones += (n&1);
         n>>1;// 右移一位
     }
+    // cout<<ones<<endl;
     return ones;
 }// 该算法的复杂度为O(logn)
 
@@ -17,5 +21,19 @@ int countOnes1opti(unsigned int n)
         ones += 1;
         n &= n-1;// 很巧妙，与最右的1减一之后求与，直接去掉了这个1
     }// 复杂度为n中1的个数
-    
+    return ones;
+}
+
+// 效率更高的算法
+int countoneslopti1(unsigned int n)
+{
+    return 0;
+}
+
+int main(){
+    cout<<"nishiwodeer";
+    unsigned int testnum = 100;
+    int result = countOnes1(testnum);
+    cout<<result;
+    return 0;
 }
